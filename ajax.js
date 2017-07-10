0fffd08b6ff0f00533bc7cc27bd7270082cd81cf
Don Cooper <don_cooper@hotmail.com>
2017-07-10 15:59:44 -0400

    Step 1 complete

--- ajax.js
+++ ajax.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  /* Your code goes here */
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/',
+    method: 'GET',
+    data: {},
+    dataType: 'text'
+  });
 
 });
